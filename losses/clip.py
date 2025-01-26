014dc07b1743553f561e7e085fe3b42225ae9000
shsu04 <stanleyhsu1021@gmail.com>
2025-01-26 14:23:34 +0000

    images

--- losses/clip.py
+++ losses/clip.py
@@ -8,39 +8,6 @@ class CLIPLoss(nn.Module):
         super().__init__()
         self.temperature = nn.Parameter(torch.tensor(1.0))
 
-    def forward(
-        self,
-        x_1: torch.Tensor,
-        x_2: torch.Tensor,
-    ) -> dict[str, float]:
-        """
-        Computes CLIP loss on two embeddings, x_1 and x_2. Both of shape [B, C, T]
-
-        Returns:
-            clip_loss: torch.Tensor, shape [B]
-            metrics: dict[str, float]
-        """
-        assert x_1.size() == x_2.size()
-        B, C, T = x_1.size()
-        # [B, C, T] -> [B * T, C]
-        x_1, x_2 = (x_1.reshape(B * T, C), x_2.reshape(B * T, C))
-
-        x_1_norm = x_1 / (x_1.norm(dim=1, keepdim=True) + 1e-8)
-        x_2_norm = x_2 / (x_2.norm(dim=1, keepdim=True) + 1e-8)
-
-        logits = x_1_norm @ x_2_norm.transpose(0, 1)  # [B*T, B*T]
-        logits = logits / self.temperature
-
-        # Diagonal targets
-        targets = torch.arange(B * T, device=x_1.device)  # [B]
-        probs = F.log_softmax(logits, dim=-1)  # [B]
-        clip_loss = F.cross_entropy(probs, targets, reduction="mean")
-
-        return {
-            "loss": clip_loss,  # Still on device
-            "metrics": self.eval_metrics(probs, targets),  # on CPU
-        }
-
     # def forward(
     #     self,
     #     x_1: torch.Tensor,
@@ -48,19 +15,24 @@ class CLIPLoss(nn.Module):
     # ) -> dict[str, float]:
     #     """
     #     Computes CLIP loss on two embeddings, x_1 and x_2. Both of shape [B, C, T]
+
     #     Returns:
     #         clip_loss: torch.Tensor, shape [B]
     #         metrics: dict[str, float]
     #     """
     #     assert x_1.size() == x_2.size()
+    #     B, C, T = x_1.size()
+    #     # [B, C, T] -> [B * T, C]
+    #     x_1, x_2 = (x_1.reshape(B * T, C), x_2.reshape(B * T, C))
 
-    #     inv_norms = 1 / (1e-8 + x_1.norm(dim=(1, 2), p=2))  # [B]
+    #     x_1_norm = x_1 / (x_1.norm(dim=1, keepdim=True) + 1e-8)
+    #     x_2_norm = x_2 / (x_2.norm(dim=1, keepdim=True) + 1e-8)
 
-    #     # Compute similarity, [B, C, T] x [B, C, T] -> [B, B]
-    #     logits = torch.einsum("bct,dct,d->bd", x_1, x_2, inv_norms) / self.temperature
+    #     logits = x_1_norm @ x_2_norm.transpose(0, 1)  # [B*T, B*T]
+    #     logits = logits / self.temperature
 
     #     # Diagonal targets
-    #     targets = torch.arange(x_1.size(0), device=x_1.device)
+    #     targets = torch.arange(B * T, device=x_1.device)  # [B]
     #     probs = F.log_softmax(logits, dim=-1)  # [B]
     #     clip_loss = F.cross_entropy(probs, targets, reduction="mean")
 
@@ -68,6 +40,34 @@ class CLIPLoss(nn.Module):
     #         "loss": clip_loss,  # Still on device
     #         "metrics": self.eval_metrics(probs, targets),  # on CPU
     #     }
+
+    def forward(
+        self,
+        x_1: torch.Tensor,
+        x_2: torch.Tensor,
+    ) -> dict[str, float]:
+        """
+        Computes CLIP loss on two embeddings, x_1 and x_2. Both of shape [B, C, T]
+        Returns:
+            clip_loss: torch.Tensor, shape [B]
+            metrics: dict[str, float]
+        """
+        assert x_1.size() == x_2.size()
+
+        inv_norms = 1 / (1e-8 + x_1.norm(dim=(1, 2), p=2))  # [B]
+
+        # Compute similarity, [B, C, T] x [B, C, T] -> [B, B]
+        logits = torch.einsum("bct,dct,d->bd", x_1, x_2, inv_norms) / self.temperature
+
+        # Diagonal targets
+        targets = torch.arange(x_1.size(0), device=x_1.device)
+        probs = F.log_softmax(logits, dim=-1)  # [B]
+        clip_loss = F.cross_entropy(probs, targets, reduction="mean")
+
+        return {
+            "loss": clip_loss,  # Still on device
+            "metrics": self.eval_metrics(probs, targets),  # on CPU
+        }
 
     def eval_metrics(
         self,
