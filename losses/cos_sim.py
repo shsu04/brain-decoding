19f11208a0d13de83c39534828ea7a4bb64e9141
shsu04 <stanleyhsu1021@gmail.com>
2025-02-04 19:34:00 +0000

    checkpoint

--- losses/cos_sim.py
+++ losses/cos_sim.py
@@ -3,12 +3,11 @@ import torch
 
 def cosine_similarity_loss(pred: torch.Tensor, target: torch.Tensor):
     """
-    Compute the cosine similarity loss. Input and target of shape [B, T, C].
+    Compute the cosine similarity loss. Input and target of shape [B, T, C]
+    across T.
     """
-    pred = pred.reshape(pred.size(0), -1)  # [B, T * C]
-    target = target.reshape(target.size(0), -1)  # [B, T * C]
 
-    cosine_similarity = torch.nn.CosineSimilarity(dim=1, eps=1e-6)
+    cosine_similarity = torch.nn.CosineSimilarity(dim=-1, eps=1e-6)
     loss = 1 - cosine_similarity(pred, target)  # [B]
 
     return loss.mean()
